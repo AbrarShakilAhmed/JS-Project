a84a80e5b4f3653689933ce952479a5deb6419b0
abrarshakilahmed <skshakilppki123@gmail.com>
2025-07-21 12:03:45 +0600

    doing

--- Temp/app.js
+++ Temp/app.js
@@ -1,6 +1,16 @@
-let arr = [1, 243, 3, 43, 24, 2, 34]
+class product {
+  static product = 0
+  constructor(price, name) {
+    ;(this.name = name), (this.price = price), product.product++
+  }
+  print() {
+    console.log(`your product is ${this.name} and price ${this.price}`)
+  }
+}
 
-arr.reduce((acc, e) => {
-  console.log(acc + e)
-  return acc
-})
+let product1 = new product(12, 'df')
+let product2 = new product(12, 'df')
+let product3 = new product(12, 'df')
+let product4 = new product(12, 'df')
+
+console.log(product.product)
