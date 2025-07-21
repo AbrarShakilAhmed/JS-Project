4b39992a381c8dcce9b9a84f304615e549893c84
abrarshakilahmed <skshakilppki123@gmail.com>
2025-07-21 12:11:34 +0600

    doing

--- Temp/app.js
+++ Temp/app.js
@@ -1,16 +1,30 @@
-class product {
-  static product = 0
-  constructor(price, name) {
-    ;(this.name = name), (this.price = price), product.product++
-  }
-  print() {
-    console.log(`your product is ${this.name} and price ${this.price}`)
+// class product {
+//   static product = 0
+//   constructor(price, name) {
+//     ;(this.name = name), (this.price = price), product.product++
+//   }
+//   print() {
+//     console.log(`your product is ${this.name} and price ${this.price}`)
+//   }
+// }
+
+// let product1 = new product(12, 'df')
+// let product2 = new product(12, 'df')
+// let product3 = new product(12, 'df')
+// let product4 = new product(12, 'df')
+
+// console.log(product.product)
+
+class Animal {
+  run() {
+    console.log(`${this.name} is amnimal is running`)
   }
 }
 
-let product1 = new product(12, 'df')
-let product2 = new product(12, 'df')
-let product3 = new product(12, 'df')
-let product4 = new product(12, 'df')
+class Rabbit extends Animal {
+  name = 'rabbit'
+}
 
-console.log(product.product)
+let bunny = new Rabbit()
+
+bunny.run()
